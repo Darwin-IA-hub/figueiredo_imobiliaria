2ef430025d9640e5c3b84fc2abdbdf51cdbd5773
Ítalo Béllo <italo.bello10@gmail.com>
2025-09-16 10:48:53 -0300

    preco_cliente

--- back-end/models/interesse.go
+++ back-end/models/interesse.go
@@ -4,7 +4,7 @@ type Interesse struct {
 	IdInteresse                int    `db:"idinteresse" json:"idInteresse"`
 	InteresseAtual             string `db:"interesseatual" json:"fluxo_do_cliente"`
 	CidadeInteresse            string `db:"cidadeinteresse" json:"cidadeInteresse"`
-	IntervaloPreco             string `db:"intervalopreco" json:"intervaloPreco"`
+	IntervaloPreco             string `db:"intervalopreco" json:"preco_do_cliente"`
 	Observacao                 string `db:"observacao" json:"observacao"`
 	TipoImovelInteresse        string `db:"tipoimovelinteresse" json:"tipoImovelInteresse"`
 	Fk_Cliente_telefone        string `db:"fk_cliente_telefone" json:"fk_Cliente_telefone"`
