7b9bcecaf7cde83650c631d4d06ced9241947560
Ítalo Béllo <italo.bello10@gmail.com>
2025-09-16 15:22:33 -0300

    fix: standardize JSON tag for TipoImovelInteresse in Interesse struct

--- back-end/models/interesse.go
+++ back-end/models/interesse.go
@@ -6,7 +6,7 @@ type Interesse struct {
 	CidadeInteresse            string `db:"cidadeinteresse" json:"cidade_de_interesse"`
 	IntervaloPreco             string `db:"intervalopreco" json:"preco_do_cliente"`
 	Observacao                 string `db:"observacao" json:"observacao"`
-	TipoImovelInteresse        string `db:"tipoimovelinteresse" json:"tipoImovelInteresse"`
+	TipoImovelInteresse        string `db:"tipoimovelinteresse" json:"tipo_do_imovel"`
 	Fk_Cliente_telefone        string `db:"fk_cliente_telefone" json:"telefone_do_cliente"`
 	Fk_Imovel_idImovel         int    `db:"fk_imovel_idimovel" json:"fk_imovel_idimovel"`
 	Fk_Lancamento_idLancamento int    `db:"fk_lancamento_idlancamento" json:"fk_lancamento_idlancamento"`
