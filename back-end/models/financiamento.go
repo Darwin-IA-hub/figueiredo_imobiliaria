f91bc12e0ed61311926d21b3e9d69b000ed66d0c
Ítalo Béllo <italo.bello10@gmail.com>
2025-09-16 15:12:58 -0300

    fix: update JSON tag for Fk_Cliente_telefone in Financiamento struct

--- back-end/models/financiamento.go
+++ back-end/models/financiamento.go
@@ -3,5 +3,5 @@ package models
 type Financiamento struct {
 	IdFinanciamento        int    `db:"idfinanciamento" json:"idFinanciamento"`
 	DescricaoFinanciamento string `db:"descricaofinanciamento" json:"tipoFinanciamento"`
-	Fk_Cliente_telefone    string `db:"fk_cliente_telefone" json:"fk_Cliente_telefone"`
+	Fk_Cliente_telefone    string `db:"fk_cliente_telefone" json:"telefone_do_cliente"`
 }
