ec24dc280288920a66eb689a16c54128570149f2
Ítalo Béllo <italo.bello10@gmail.com>
2025-09-17 12:11:19 -0300

    ASD

--- back-end/models/conjuge.go
+++ back-end/models/conjuge.go
@@ -1,7 +1,5 @@
 package models
 
-import "time"
-
 type Conjuge struct {
 	IdConjuge               int     `db:"idconjuge" json:"idConjuge"`
 	RendaBrutaMensalConjuge float64 `db:"rendabrutamensalconjuge" json:"renda_bruta_do_cliente_conjuge"`
