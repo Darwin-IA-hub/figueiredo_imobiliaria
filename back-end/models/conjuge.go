1e19e8da5ffc7232bb364a0a0f99f102c0b2c3cd
Ítalo Béllo <italo.bello10@gmail.com>
2025-09-17 12:09:59 -0300

    2

--- back-end/models/conjuge.go
+++ back-end/models/conjuge.go
@@ -1,8 +1,10 @@
 package models
 
+import "time"
+
 type Conjuge struct {
-	IdConjuge               int     `db:"idconjuge" json:"idConjuge"`
-	RendaBrutaMensalConjuge float64 `db:"rendabrutamensalconjuge" json:"renda_bruta_do_cliente_conjuge"`
-	DataNascimentoConjuge   string  `db:"datanascimentoconjuge" json:"data_de_nascimento_conjuge"`
-	Fk_Cliente_telefone     string  `db:"fk_cliente_telefone" json:"telefone_do_cliente"`
+	IdConjuge               int        `db:"idconjuge" json:"idConjuge"`
+	RendaBrutaMensalConjuge float64    `db:"rendabrutamensalconjuge" json:"renda_bruta_do_cliente_conjuge"`
+	DataNascimentoConjuge   *time.Time `db:"datanascimentoconjuge" json:"data_de_nascimento_conjuge"`
+	Fk_Cliente_telefone     string     `db:"fk_cliente_telefone" json:"telefone_do_cliente"`
 }
