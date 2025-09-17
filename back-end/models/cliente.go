e322298cb06e58bec642f2c759828c2861e0a1b7
Ítalo Béllo <italo.bello10@gmail.com>
2025-09-17 11:41:48 -0300

    a

--- back-end/models/cliente.go
+++ back-end/models/cliente.go
@@ -7,7 +7,7 @@ type Cliente struct {
 	RendaBrutaCliente     float64 `db:"rendabrutacliente" json:"renda_bruta_do_cliente"`
 	QuantidadeFilhos      int     `db:"quantidadefilhos" json:"filhos_quantidade"`
 	AnosCarteiraAssinada  int     `db:"anoscarteiraassinada" json:"quantos_anos_de_carteira"`
-	TeveSubsidio          string  `db:"tevesubsidio" json:"tem_outros_subsidios"`
-	VaiUsarFGTS           string  `db:"vaiusarfgts" json:"fgts"`
-	PossuiFinanciamento   string  `db:"possuifinanciamento" json:"possui_financiamento"`
+	TeveSubsidio          bool    `db:"tevesubsidio" json:"tem_outros_subsidios"`
+	VaiUsarFGTS           bool    `db:"vaiusarfgts" json:"fgts"`
+	PossuiFinanciamento   bool    `db:"possuifinanciamento" json:"possui_financiamento"`
 }
